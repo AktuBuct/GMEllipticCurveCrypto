cd19ff02683e200daf8f1a6bc76b47ac68328e20
Zelikson Stanislav <zelikson@yamoney.ru>
2020-07-24 15:13:43 +0300

    111

---
+++ Example/build/Pods.build/Debug-iphonesimulator/GMEllipticCurveCrypto.build/DerivedSources/GMEllipticCurveCrypto_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char GMEllipticCurveCryptoVersionString[];
+ extern const double GMEllipticCurveCryptoVersionNumber;
+
+ const unsigned char GMEllipticCurveCryptoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GMEllipticCurveCrypto  PROJECT:Pods-1" "\n";
+ const double GMEllipticCurveCryptoVersionNumber __attribute__ ((used)) = (double)1.;
